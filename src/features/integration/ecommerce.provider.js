42a81d432153def35adea4d5211cc8de2caebc65
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-12-07 12:55:25 -0300

    teste

--- src/features/integration/ecommerce.provider.js
+++ src/features/integration/ecommerce.provider.js
@@ -213,7 +213,8 @@ class EcommerceProvider {
             brechoId: peca.id,
             images: images,
             is_variable: attributes.length > 0, // Mark as variable if it has attributes
-            measurements: peca.medidas // Pass measurements array
+            measurements: peca.medidas, // Pass measurements array
+            supplier: peca.fornecedor?.nome // Pass supplier name
         };
 
         // If it has attributes, we should create a default variation so it appears in filters
